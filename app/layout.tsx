de94ddfa843be5d59e8ac68adf2928aa760ab058
Kailash Kumar <kailash@kailashs-MacBook-Air.local>
2024-03-07 14:08:19 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
+ });
+
+export const metadata: Metadata = {
+  title: "Imaginator",
+  description: "AI-Powered Image Generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+    <html lang="en">
+      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
